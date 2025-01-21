b6e293feaa45bf0e86d5eef57f7d1242add5b618
tdogcodes <trxcycsgo@gmail.com>
2025-01-20 19:57:56 -0800

    first commit

---
+++ starter-template/src/sections/Hero.tsx
@@ -0,0 +1,70 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import Image from "next/image";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+
+export const HeroSection = () => {
+  return ( 
+  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="absolute inset-0 -z-30 opacity-5" style={{
+      backgroundImage: `url(${grainImage.src})`,
+    }}></div>
+    <div className="size-[620px] hero-ring"></div>
+    <div className="size-[820px] hero-ring"></div>
+    <div className="size-[1020px] hero-ring"></div>
+    <div className="size-[1220px] hero-ring"></div>
+    <HeroOrbit size={800} rotation={282}>
+          <StarIcon className="size-28 text-emerald-300" />
+    </HeroOrbit>
+    <HeroOrbit size={660} rotation={90}>
+          <StarIcon className="size-12 text-emerald-300" />
+    </HeroOrbit>
+    <HeroOrbit size={830} rotation={0}>
+          <StarIcon className="size-28 text-blue-300/70" />
+    </HeroOrbit>
+    <HeroOrbit size={420} rotation={-10}>
+          <StarIcon className="size-8 text-emerald-300" />
+    </HeroOrbit>
+    <HeroOrbit size={850} rotation={-40}>
+          <StarIcon className="size-16 text-blue-300/70" />
+    </HeroOrbit>
+    <HeroOrbit size={530} rotation={160}>
+          <SparkleIcon className="size-8 text-blue-300/20" />
+    </HeroOrbit>
+    <HeroOrbit size={800} rotation={-170}>
+          <SparkleIcon className="size-16 text-emerald-300/40" />
+    </HeroOrbit>
+    <HeroOrbit size={700} rotation={125}>
+          <SparkleIcon className="size-14 text-blue-300/60" />
+    </HeroOrbit>
+    <div className="container">
+      <div className="flex flex-col items-center">
+        <div className="bg-gray-950 border border-gray-950 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="text-sm font-medium">Available for new projects</div>
+        </div>
+      </div>
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-6 tracking-wide">Building Exceptional User Experiences</h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">
+          I specialize in creating web apps using React, Next.js, Tailwind, and Typescript
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="text-semibold">
+            View Projects</span>
+          <ArrowDown className="size-4"/>
+        </button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl ">
+          <span>🤝</span>
+          <span className="font-semibold">Lets Connect</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  );
+};
